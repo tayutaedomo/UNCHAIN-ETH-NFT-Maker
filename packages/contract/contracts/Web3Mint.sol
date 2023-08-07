d46320e680fe7c52171f526076be073d60003654
tayutaedomo <tayutaedomo@gmail.com>
2023-08-07 09:11:16 +0900

    Section 1 Lesson 3 is completed

---
+++ packages/contract/contracts/Web3Mint.sol
@@ -0,0 +1,11 @@
+// Web3Mint.sol
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.18;
+
+import "hardhat/console.sol";
+
+contract Web3Mint {
+    constructor() {
+        console.log("This is my NFT contract.");
+    }
+}
---
+++ packages/contract/scripts/run.js
@@ -0,0 +1,19 @@
+// run.js
+const main = async () => {
+  const nftContractFactory = await hre.ethers.getContractFactory("Web3Mint");
+  const nftContract = await nftContractFactory.deploy();
+  await nftContract.deployed();
+  console.log("Contract deployed to:", nftContract.address);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runMain();

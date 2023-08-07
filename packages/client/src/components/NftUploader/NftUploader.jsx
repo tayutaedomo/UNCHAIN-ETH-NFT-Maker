2779f8d0dc57d2bd1a7e1f57304cbc420e2fbce4
tayutaedomo <tayutaedomo@gmail.com>
2023-08-08 01:42:04 +0900

    Section 3 Lesson 2 is completed

--- packages/client/src/components/NftUploader/NftUploader.jsx
+++ packages/client/src/components/NftUploader/NftUploader.jsx
@@ -1,7 +1,13 @@
 import { Button } from "@mui/material";
 import React, { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import { Web3Storage } from "web3.storage";
+import Web3Mint from "../../utils/Web3Mint.json";
 import ImageLogo from "./image.svg";
 import "./NftUploader.css";
+
+const CONTRACT_ADDRESS = process.env.REACT_APP_CONTRACT_ADDRESS;
+const API_KEY = process.env.REACT_APP_WEB3_STORAGE_TOKEN;
 
 const NftUploader = () => {
   const [currentAccount, setCurrentAccount] = useState("");
@@ -44,6 +50,46 @@ const NftUploader = () => {
     }
   };
 
+  const imageToNFT = async (e) => {
+    const client = new Web3Storage({ token: API_KEY });
+    const image = e.target;
+    console.log(image);
+    const rootCid = await client.put(image.files, {
+      name: 'experiment',
+      maxRetries: 3,
+    });
+    const res = await client.get(rootCid);
+    const files = await res.files();
+    for (const file of files) {
+      console.log("file.cid", file.cid);
+      askContractToMintNft(file.cid);
+    }
+  };
+
+  const askContractToMintNft = async (ipfs) => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum); // v5
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(
+          CONTRACT_ADDRESS,
+          Web3Mint.abi,
+          signer
+        );
+        console.log("Going to pop wallet now to pay gas...");
+        const nftTxn = await connectedContract.makeAnEpicNFT("sample", ipfs);
+        console.log("Mining...please wait");
+        await nftTxn.wait();
+        console.log(`Mined, see transaction: https://sepolia.etherscan.io/tx/${nftTxn.hash}`);
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const renderNotConnectedContainer = () => (
     <button onClick={connectWallet} className="cta-button connect-wallet-button">
       Connect to Wallet
@@ -70,12 +116,12 @@ const NftUploader = () => {
           <img src={ImageLogo} alt="imagelogo" />
           <p>ここにドラッグ＆ドロップしてね</p>
         </div>
-        <input className="nftUploadInput" multiple name="imageURL" type="file" accept=".jpg , .jpeg , .png"  />
+        <input className="nftUploadInput" multiple name="imageURL" type="file" accept=".jpg , .jpeg , .png"  onChange={imageToNFT} />
       </div>
       <p>または</p>
       <Button variant="contained">
         ファイルを選択
-        <input className="nftUploadInput" type="file" accept=".jpg , .jpeg , .png"/>
+        <input className="nftUploadInput" type="file" accept=".jpg , .jpeg , .png" onChange={imageToNFT} />
       </Button>
     </div>
   );
